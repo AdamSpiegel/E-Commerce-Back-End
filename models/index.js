76c23326dbfc6a1cba99bbd326751f6cf65d55ff
Your Name <you@example.com>
2021-06-18 18:06:58 -0400

    Updating Database Models

--- models/index.js
+++ models/index.js
@@ -5,10 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+
+})
 
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+
 
 // Tags belongToMany Products (through ProductTag)
 
